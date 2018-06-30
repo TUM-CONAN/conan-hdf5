10454148675ae5c3141cc163675e8371103603de
Intelligide <intelligide@hotmail.fr>
2018-06-30 19:32:44 +0200

    Use short paths

--- conanfile.py
+++ conanfile.py
@@ -23,6 +23,7 @@ class Hdf5Conan(ConanFile):
         "shared": [True, False]
     }
     default_options = "shared=False"
+    short_paths = True
 
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
